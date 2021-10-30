54842530675404a97a74d7bbdde7550ff53731b1
Wolf <wolf2wulf@gmail.com>
2021-10-30 14:55:49 +0200

    fixed veripb install script

--- setup_veripb.sh
+++ setup_veripb.sh
@@ -5,12 +5,12 @@
 
 # Unrar
 cd veripb
-unrar x -ad veripb-54cf2c28b1ebea7292dc618f213878d716644bc3.rar 
-cd veripb-54cf2c28b1ebea7292dc618f213878d716644bc3
+unrar x -ad veripb.rar
+cd veripb
 
 # Build 
 pip3 install ./
 
 # Clean up
 cd ..
-rm -rf veripb-54cf2c28b1ebea7292dc618f213878d716644bc3 
+rm -rf veripb
