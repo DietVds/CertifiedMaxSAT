32419a8b30f333767d734162115797ac90487d6a
Dieter <dieter.vandesande@vub.be>
2022-02-11 15:16:37 +0100

    add log_duration_gencardinals for the baseline qmaxsat

--- scripts/localtest/test.sh
+++ scripts/localtest/test.sh
@@ -7,19 +7,17 @@ let MEMOUT_SOLVER=8192
 let MEMOUT_SOLVER_PL=MEMOUT_SOLVER
 let MEMOUT_VERIPB=1*$MEMOUT_SOLVER_PL
 
-file_results="results.txt"
+file_results="results.csv"
 TMPDIR=tmpdir
 instances="instances_to_test"
 
-echo "filename,runtime_without_prooflogging,runtime_with_prooflogging,runtime_verification, verification_succeeded" > ./$(echo file_results)
-echo "instances, filename, res_runtime_without_prooflogging, res_mem_without_prooflogging, res_runtime_with_prooflogging, res_mem_with_prooflogging, res_runtime_verification, res_mem_verification, res_verification_succeeded" > ./$(echo file_results)
-
-for filename in $(ls mse21_complete_unwt/easy_instances/)
+echo "instances, filename, res_runtime_without_prooflogging, res_time_genCardinals_without_PL, res_mem_without_prooflogging, res_runtime_with_prooflogging, res_time_genCardinals, res_time_genCardinalDefinitions,  res_mem_with_prooflogging, res_runtime_verification, res_mem_verification, res_verification_succeeded" > ./$(echo $file_results)
+for filename in $(ls $instances)
 do
 	gunzip ./instances_to_test/$filename
 done
 
-for filename in $(ls "$instances")
+for filename in $(ls $instances)
 do
 	echo "start $filename"
 	
@@ -32,7 +30,7 @@ do
 	res_verification_succeeded="NA"
 
 	# run
-	runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $TMPDIR/$filename.txt ./qmaxsat_without_prooflogging ./instances_to_test/$filename 
+	runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $TMPDIR/$filename.txt ./../../qmaxsat_without_PL -log_duration_totalizer ./instances_to_test/$filename 
 	
 	# extract time
 	res_runtime_without_prooflogging=$(cat $TMPDIR/$filename.txt | grep 'real:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
@@ -40,12 +38,16 @@ do
 	# extract space
 	res_mem_without_prooflogging=$(cat $TMPDIR/$filename.txt | grep 'space:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
 
+	#extract time for genCardinals
+	res_time_genCardinals_without_PL=$(cat duration_totalizer.txt | grep 'genCardinals:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
+	    		
 	# status
 	status=$(cat $TMPDIR/$filename.txt | grep 'status:' | awk '{print $3}');
 
 	echo "$filename without prooflogging:"
 	echo "runtime: $res_runtime_without_prooflogging "
 	echo "mem: $res_mem_without_prooflogging"
+	echo "runtime genCardinals: $res_time_genCardinals_without_PL"
 
 	if [[ "$status" == "ok" ]]
 	then
@@ -63,11 +65,12 @@ do
 	    	# status
 	    	status=$(cat $TMPDIR/$filename.txt | grep 'status:' | awk '{print $3}');
 	    	
-	    	#duration genCardinalDefinitions
+	    	#extract time for genCardinals
+	    	res_time_genCardinals=$(cat duration_totalizer.txt | grep 'genCardinals:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
+	    		    	
+	    	#extract time for genCardinalDefinitions
 	    	res_time_genCardinalDefinitions=$(cat duration_totalizer.txt | grep 'genCardinalDefinitions:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
 	    	
-	    	#duration genCardinals
-	    	res_time_genCardinals=$(cat duration_totalizer.txt | grep 'genCardinals:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
 	    		    	
 	    	echo "$filename with prooflogging:"
 		echo "runtime: $res_runtime_with_prooflogging "
@@ -101,6 +104,6 @@ do
 			fi
 		fi
 	fi
-	echo "$instances, $filename, $res_runtime_without_prooflogging, $res_mem_without_prooflogging, $res_runtime_with_prooflogging, $res_time_genCardinalDefinitions, $res_time_genCardinals, $res_mem_with_prooflogging, $res_runtime_verification, $res_mem_verification, $res_verification_succeeded" >> ./$(echo file_results)
+	echo "$instances, $filename, $res_runtime_without_prooflogging, $res_time_genCardinals_without_PL, $res_mem_without_prooflogging, $res_runtime_with_prooflogging, $res_time_genCardinals, $res_time_genCardinalDefinitions,  $res_mem_with_prooflogging, $res_runtime_verification, $res_mem_verification, $res_verification_succeeded" >> ./$(echo $file_results)
 done
 
