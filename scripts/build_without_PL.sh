8e4e68742a75213bf12b22e29a0feb995663b30f
Dieter <dieter.vandesande@vub.be>
2022-02-11 14:57:46 +0100

    create script to build qmaxsat without prooflogging containing the parameter log_duration_totalizer

---
+++ scripts/build_without_PL.sh
@@ -0,0 +1,19 @@
+#
+# Script that extracts Minisat and QMaxSAT, merges them, applies the patches to them and builds the solver
+#
+ROOT_DIR=$(pwd)
+
+# Build the source code
+./scripts/build_src.sh ./src_tmp
+
+# Apply the proof logging patch
+cd src_tmp
+patch -p1 < $ROOT_DIR/patches/log_duration_gencard_without_PL.patch
+
+# Build, cleanup and move binary to root
+cd core
+make
+rm *.o
+mv qmaxsat ../../qmaxsat_without_PL
+cd $ROOT_DIR
+rm -rf src_tmp
