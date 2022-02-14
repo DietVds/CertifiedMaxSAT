502119fa41140a2c646e312905ed9269c2f0d606
Dieter <dieter.vandesande@vub.be>
2022-02-14 09:20:03 +0100

    add proofsize to testscript

--- scripts/localtest/test.sh
+++ scripts/localtest/test.sh
@@ -11,7 +11,7 @@ file_results="results.csv"
 TMPDIR=tmpdir
 instances="instances_to_test"
 
-echo "instances, filename, res_runtime_without_prooflogging, res_time_genCardinals_without_PL, res_mem_without_prooflogging, res_runtime_with_prooflogging, res_time_genCardinals, res_time_genCardinalDefinitions,  res_mem_with_prooflogging, res_runtime_verification, res_mem_verification, res_verification_succeeded" > ./$(echo $file_results)
+echo "instances, filename, res_runtime_without_prooflogging, res_time_genCardinals_without_PL, res_mem_without_prooflogging, res_runtime_with_prooflogging, res_proofsize, res_time_genCardinals, res_time_genCardinalDefinitions,  res_mem_with_prooflogging, res_runtime_verification, res_mem_verification, res_verification_succeeded" > ./$(echo $file_results)
 for filename in $(ls $instances)
 do
 	gunzip ./instances_to_test/$filename
@@ -28,7 +28,13 @@ do
 	res_runtime_verification="NA"
 	res_mem_verification="NA"
 	res_verification_succeeded="NA"
-
+	res_time_genCardinals_without_PL="NA"
+	res_time_genCardinals="NA"
+	res_time_genCardinalDefinitions="NA"
+	res_proofsize="NA"
+	
+	rm duration_totalizer.txt
+	
 	# run
 	runlim -r $TIMEOUT_SOLVER -s $MEMOUT_SOLVER -o $TMPDIR/$filename.txt ./../../qmaxsat_without_PL -log_duration_totalizer ./instances_to_test/$filename 
 	
@@ -53,6 +59,8 @@ do
 	then
 		
 		rm maxsat_proof.pbp
+		rm duration_totalizer.txt
+		
 		# run
 	    	runlim -r $TIMEOUT_SOLVER_PL -s $MEMOUT_SOLVER_PL -o $TMPDIR/$filename.txt ./../../qmaxsat -log_duration_totalizer ./$instances/$filename 
 
@@ -67,16 +75,34 @@ do
 	    	
 	    	#extract time for genCardinals
 	    	res_time_genCardinals=$(cat duration_totalizer.txt | grep 'genCardinals:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
-	    		    	
+	    	
+	    	if [[ "$res_time_genCardinals" == "" ]]
+	    	then
+	    		res_time_genCardinals="NA"
+	    	fi
+	    	   		    	
 	    	#extract time for genCardinalDefinitions
 	    	res_time_genCardinalDefinitions=$(cat duration_totalizer.txt | grep 'genCardinalDefinitions:' | grep -Eo '[+-]?[0-9]+([.][0-9]+)?');
 	    	
+	    	if [[ "$res_time_genCardinalDefinitions" == "" ]]
+	    	then
+	    		res_time_genCardinalDefinitions="NA"
+	    	fi
+	    	
+	    	# extract proof size
+	    	proofsize=$(stat --printf="%s" maxsat_proof.pbp)
+	    
+	    	if [[ "$proofsize" == "" ]]
+		then
+			proofsize="NA"
+		fi
 	    		    	
 	    	echo "$filename with prooflogging:"
 		echo "runtime: $res_runtime_with_prooflogging "
 		echo "mem: $res_mem_with_prooflogging"
 		echo "duration genCardinalDefinitions: $res_time_genCardinalDefinitions"
 		echo "duration genCardinals: $res_time_genCardinals"
+		echo "proofsize: $proofsize"
 		
 		if [[ "$status" == "ok" ]]
 		then	
@@ -104,6 +130,6 @@ do
 			fi
 		fi
 	fi
-	echo "$instances, $filename, $res_runtime_without_prooflogging, $res_time_genCardinals_without_PL, $res_mem_without_prooflogging, $res_runtime_with_prooflogging, $res_time_genCardinals, $res_time_genCardinalDefinitions,  $res_mem_with_prooflogging, $res_runtime_verification, $res_mem_verification, $res_verification_succeeded" >> ./$(echo $file_results)
+	echo "$instances, $filename, $res_runtime_without_prooflogging, $res_time_genCardinals_without_PL, $res_mem_without_prooflogging, $res_runtime_with_prooflogging, $proofsize, $res_time_genCardinals, $res_time_genCardinalDefinitions,  $res_mem_with_prooflogging, $res_runtime_verification, $res_mem_verification, $res_verification_succeeded" >> ./$(echo $file_results)
 done
 
