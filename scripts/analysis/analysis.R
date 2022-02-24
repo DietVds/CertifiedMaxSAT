f82045c8d5deef78bb594b0904fd2a6bce26fd19
Wolf <wolf2wulf@gmail.com>
2022-02-24 12:42:13 +0100

    added safety checks to analysis code and updated cluster scripts

--- scripts/analysis/analysis.R
+++ scripts/analysis/analysis.R
@@ -10,11 +10,23 @@ instances <- results$instance
 results <- as.data.frame(lapply(results, as.numeric))
 results$instance <- instances
 
+# Safety checks
+mem_limit <- 40960
+time_limit <- 36000
+for (row in 1:nrow(results)) {
+    if (!is.na(results[row, "status"]) & results[row, "status"] == 0 & results[row, "runtime_v"] < time_limit & results[row, "mem_v"] < mem_limit) {
+        print(results[row, ])
+    }
+}
+
 # QMaxSATpb OOTs
 for (row in 1:nrow(results)) {
     if (!is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] >= 3600) {
         results[row, "runtime_w"] <- 4000
         results[row, "proofsize"] <- 0
+    }
+    if (!is.na(results[row, "runtime"]) & !is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] < results[row, "runtime"]) {
+        print(results[row, ])
     }
 }
 
@@ -50,16 +62,18 @@ for (row in 1:nrow(results)) {
 # TYPE1
 no_NAs <- results[!is.na(results$runtime) & results$runtime < 3600 & !is.na(results$mem) & results$mem < 32768, ]
 
-ggplot(no_NAs, aes(x = runtime_w, y = runtime, color = log10((proofsize / 10^3) + 1))) +
+ggplot(no_NAs, aes(x = runtime_w, y = runtime, color = log10(mem_w + 1))) +
+    # ggplot(no_NAs, aes(x = runtime_w, y = runtime, color = log10((proofsize / 10^3) + 1))) +
     geom_point() +
     scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) +
     scale_y_log10(breaks = c(1, 10, 100, 1000, 10000)) +
-    scale_color_continuous(breaks = c(3, 6, 9), labels = c("1MB", "1GB", "1TB")) +
+    scale_color_continuous(breaks = c(1, 2, 3, 4, 5, 6), labels = c("10MB", "100MB", "1GB", "10GB", "100GB")) +
+    # scale_color_continuous(breaks = c(3, 6, 9), labels = c("1MB", "1GB", "1TB")) +
     coord_fixed(ratio = 1) +
     geom_vline(xintercept = 4000, linetype = "dashed") +
     geom_vline(xintercept = 8500, linetype = "dashed") +
     geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
-    labs(color = "Proofsize") +
+    labs(color = "Memory used") +
     xlab("QMaxSATpb (time in s)") +
     ylab("QMaxSAT (time in s)") +
     coord_cartesian(xlim = c(0.1, 10000), ylim = c(0.1, 10000)) +
@@ -86,18 +100,20 @@ ggsave("./scripts/analysis/without_vs_with.pdf", device = "pdf", width = 14, hei
 # TYPE2
 no_NAs2 <- results[!is.na(results$runtime) & results$runtime < 3600 & !is.na(results$mem) & results$mem < 32768, ]
 
-ggplot(no_NAs2, aes(x = runtime_v, y = runtime_w, color = log10((proofsize / 10^3) + 1))) +
+ggplot(no_NAs2, aes(x = runtime_v, y = runtime_w, color = log10(mem_v + 1))) +
+    # ggplot(no_NAs, aes(x = runtime_w, y = runtime, color = log10((proofsize / 10^3) + 1))) +
     geom_point() +
     scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) +
     scale_y_log10(breaks = c(1, 10, 100, 1000, 10000)) +
-    scale_color_continuous(breaks = c(3, 6, 9), labels = c("1MB", "1GB", "1TB")) +
+    scale_color_continuous(breaks = c(1, 2, 3, 4, 5), labels = c("10MB", "100MB", "1GB", "10GB", "100GB")) +
+    # scale_color_continuous(breaks = c(3, 6, 9), labels = c("1MB", "1GB", "1TB")) +
     coord_fixed(ratio = 1) +
     geom_vline(xintercept = 46000, linetype = "dashed") +
     geom_vline(xintercept = 99000, linetype = "dashed") +
     geom_abline(slope = 1, intercept = 0, linetype = "dashed") +
     xlab("VeriPB (time in s)") +
     ylab("QMaxSATpb (time in s)") +
-    labs(color = "Proofsize") +
+    labs(color = "Memory used") +
     coord_cartesian(xlim = c(0.1, 100000), ylim = c(0.1, 10000)) +
     annotate(
         geom = "text",
