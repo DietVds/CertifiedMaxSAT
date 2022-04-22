c5b2d334b3bef5f5fd0bf0eff06f4fb547f56408
wulfdewolf <wolf.de.wulf@vub.be>
2022-04-22 14:10:58 +0200

    results

--- scripts/analysis/analysis.R
+++ scripts/analysis/analysis.R
@@ -32,10 +32,10 @@ for (row in 1:nrow(results)) {
     }
 
     # 
-    if (!is.na(results[row, "runtime"]) & !is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] < results[row, "runtime"] & results[row, "runtime"] < 1.5) {
-        print(results[row,])
-        write(results[row, "instance"], "weirds.txt", append = TRUE)
-    }
+    #if (!is.na(results[row, "runtime"]) & !is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] < results[row, "runtime"] & results[row, "runtime"] < 1.5) {
+    #    print(results[row,])
+    #    write(results[row, "instance"], "weirds.txt", append = TRUE)
+    #}
 }
 
 # QMaxSATpb OOTs
@@ -57,16 +57,12 @@ for (row in 1:nrow(results)) {
     if (!is.na(results[row, "runtime_v"]) & results[row, "runtime_v"] >= 10 * time_limit) {
         results[row, "runtime_v"] <- 46000
     }
-    if (!is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] < time_limit & !is.na(results[row, "mem_w"]) & results[row, "mem_w"] < mem_limit) {
-    }
 }
 
 # VeriPB OOMs
 for (row in 1:nrow(results)) {
     if (!is.na(results[row, "mem_v"]) & results[row, "mem_v"] >= 1.25 * mem_limit) {
         results[row, "runtime_v"] <- 99000
-    }
-    if (!is.na(results[row, "runtime_w"]) & results[row, "runtime_w"] < time_limit & !is.na(results[row, "mem_w"]) & results[row, "mem_w"] < mem_limit) {
     }
 }
 
@@ -83,7 +79,7 @@ ggplot(no_NAs, aes(x = runtime_w, y = runtime, color = log10((proofsize / 10^3) 
     geom_point() +
     scale_x_log10(breaks = c(1, 10, 100, 1000)) +
     scale_y_log10(breaks = c(1, 10, 100, 1000)) +
-    scale_color_continuous(breaks = c(2, 3, 4, 5, 6, 7), labels = c("100KB", "1MB", "10MB", "100MB", "1GB", "10GB")) +
+    scale_color_continuous(breaks = c(1, 2, 3, 4, 5, 6, 7), labels = c("10KB", "100KB", "1MB", "10MB", "100MB", "1GB", "10GB")) +
     coord_fixed(ratio = 1) +
     geom_vline(xintercept = 4000, linetype = "dashed") +
     geom_vline(xintercept = 8500, linetype = "dashed") +
@@ -121,7 +117,7 @@ ggplot(no_NAs2, aes(x = runtime_v, y = runtime_w, color = log10((proofsize / 10^
     geom_point() +
     scale_x_log10(breaks = c(1, 10, 100, 1000, 10000)) +
     scale_y_log10(breaks = c(1, 10, 100, 1000, 10000)) +
-    scale_color_continuous(breaks = c(2, 3, 4, 5, 6, 7), labels = c("100KB", "1MB", "10MB", "100MB", "1GB", "10GB")) +
+    scale_color_continuous(breaks = c(1, 2, 3, 4, 5, 6, 7), labels = c("10KB", "100KB", "1MB", "10MB", "100MB", "1GB", "10GB")) +
     coord_fixed(ratio = 1) +
     geom_vline(xintercept = 46000, linetype = "dashed") +
     geom_vline(xintercept = 99000, linetype = "dashed") +
